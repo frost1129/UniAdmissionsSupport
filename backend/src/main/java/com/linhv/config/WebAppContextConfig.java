09dd9004f526f79e3548f38e6c66146eb8c7a354
Linh Vu <prodige112002@gmail.com>
2023-08-10 18:32:36 +0700

    [Linh] add basic config

---
+++ backend/src/main/java/com/linhv/config/WebAppContextConfig.java
@@ -0,0 +1,23 @@
+package com.linhv.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages = {
+        "com.linhv.controllers",
+        "com.linhv.service",
+        "com.linhv.repository"
+})
+public class WebAppContextConfig implements WebMvcConfigurer {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
