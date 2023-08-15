df9932331c8bbb0d17e329e00234b15556961559
Linh Vu <prodige112002@gmail.com>
2023-08-15 22:00:51 +0700

    [Linh] init backend

---
+++ backend/src/main/java/com/linhv/controller/ApiUniInfoController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.linhv.controller;
+
+import org.apache.http.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author prodi
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiUniInfoController {
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return new ResponseEntity<>("Hello", HttpStatus.OK);
+    }
+}
