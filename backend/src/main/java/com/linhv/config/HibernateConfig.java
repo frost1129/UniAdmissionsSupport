aaeebd37fbffbd9892a3d00e846ad053601d54f7
Linh Vu <prodige112002@gmail.com>
2023-08-15 16:44:33 +0700

    [Linh] init server

--- backend/src/main/java/com/linhv/config/HibernateConfig.java
+++ backend/src/main/java/com/linhv/config/HibernateConfig.java
@@ -36,7 +36,7 @@ public class HibernateConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("hibernate.connection.username"));
+        dataSource.setDriverClassName(env.getProperty("hibernate.connection.driverClass"));
         dataSource.setUrl(env.getProperty("hibernate.connection.url"));
         dataSource.setUsername(env.getProperty("hibernate.connection.username"));
         dataSource.setPassword(env.getProperty("hibernate.connection.password"));
