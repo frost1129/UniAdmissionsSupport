cfe713f6ac5122092fe169e1308fdef3fa4ded16
Linh Vu <prodige112002@gmail.com>
2023-08-16 21:59:12 +0700

    [Linh] init backend

--- backend/src/main/java/com/linhv/controller/ApiUniInfoController.java
+++ backend/src/main/java/com/linhv/controller/ApiUniInfoController.java
@@ -5,7 +5,6 @@
 
 package com.linhv.controller;
 
-import org.apache.http.HttpResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
