81a2ca20fdc0ce75cd81f04c218a356d15159f81
Linh Vu <prodige112002@gmail.com>
2023-08-21 10:27:27 +0700

    [Linh] front: search component

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Button, Container, Form, InputGroup } from "react-bootstrap";
 
 const Search = () => {
-  return (
-    <Container className='bg-info bg-gradient p-3 text-center' fluid>
-            <h3>Tìm kiếm thông tin</h3>
+    return (
+        <Container className="bg-primary bg-gradient p-3 text-center" fluid>
+            <h3 className="text-center my-3">Tìm kiếm thông tin</h3>
+            <Container className="w-75 mx-auto">
+            <InputGroup className="mb-3">
+                <Form.Control
+                  placeholder="Nội dung cần tìm..."
+                  aria-label="Nội dung cần tìm..."
+                />
+                <Button variant="outline-light">
+                    Tìm kiếm
+                    <FontAwesomeIcon className="mx-1" icon={faSearch} />
+                </Button>
+            </InputGroup>
+            </Container>
+        </Container>
+    );
+};
 
-    </Container>
-  )
-}
-
-export default Search
+export default Search;
