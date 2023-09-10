4acfaac9110582f7415940d8ec61bb86403a2768
Linh Vu <prodige112002@gmail.com>
2023-09-10 16:57:35 +0700

    [Linh] posts pagination

--- frontend/src/config/Api.js
+++ frontend/src/config/Api.js
@@ -12,23 +12,30 @@ export const endpoints = {
     "admissions": `${SERVER_CONTEXT}/api/admissions/`,
     "faculties": `${SERVER_CONTEXT}/api/faculties/`,
     "topics": `${SERVER_CONTEXT}/api/topics/`,
+    
     "register": `${SERVER_CONTEXT}/api/users/`,
     "login": `${SERVER_CONTEXT}/api/login/`, 
-    "top-admission-post": (admissionId) => `${SERVER_CONTEXT}/api/posts/top-five/${admissionId}/`, 
+    "current-user": `${SERVER_CONTEXT}/api/current-user/`, 
+    
     "top-5-faqs": `${SERVER_CONTEXT}/api/faqs/top-5/`,
     "faqs-count": `${SERVER_CONTEXT}/api/faqs/counter/`, 
     "faqs": `${SERVER_CONTEXT}/api/faqs/`, 
+    
+    "top-admission-post": (admissionId) => `${SERVER_CONTEXT}/api/posts/top-five/${admissionId}/`, 
     "posts": `${SERVER_CONTEXT}/api/posts/`, 
     "posts-count": `${SERVER_CONTEXT}/api/posts/counter/`, 
     "post-details": (postId) => `${SERVER_CONTEXT}/api/posts/${postId}`, 
-    "current-user": `${SERVER_CONTEXT}/api/current-user/`, 
+    
+
+    "question-setting": `${SERVER_CONTEXT}/api/questions/setting/`, 
+
     "question-detail": (id) => `${SERVER_CONTEXT}/api/user-questions/${id}/`,
     "question-unanswer": `${SERVER_CONTEXT}/api/user-questions/unanswer/`,
     "question-admission": `${SERVER_CONTEXT}/api/user-questions/advisor/`,
     "question-email": `${SERVER_CONTEXT}/api/user-questions/by-email/`, 
-    "question-setting": `${SERVER_CONTEXT}/api/questions/setting/`, 
     "question-add": `${SERVER_CONTEXT}/api/user-question/add/`, 
     "question-update": `${SERVER_CONTEXT}/api/user-question/update/`, 
+
     "faculty-post": (id) => `${SERVER_CONTEXT}/api/faculty-post/${id}/`, 
     "faculty-score": (id) => `${SERVER_CONTEXT}/api/faculty-score/${id}/`, 
     "faculty-detail": (id) => `${SERVER_CONTEXT}/api/faculties/${id}/`, 
